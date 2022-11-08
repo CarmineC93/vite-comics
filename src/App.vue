ed6964e3cd772f878f1678ca677326ec7d24cac1
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-11-08 14:58:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import AppHeader from './components/AppHeader.vue'; 
+</script>
+
+<template>
+  <h1>Prova</h1>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+    export default {
+        nome: "AppHeader",
+        data (){
+            
+        }
+    }
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
