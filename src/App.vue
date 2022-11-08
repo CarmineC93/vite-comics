c24ef2ec0f0b53fb60537149d323b7d36ea8954b
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-11-08 16:41:02 +0100

    layout header set

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
-<script setup>
-  import AppHeader from './components/AppHeader.vue'; 
+<script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppHeader from './components/AppContent.vue';
+
+
+  export default {
+    components: {
+      AppHeader,
+    }
+  }
 </script>
 
-<template>
-  <h1>Prova</h1>
+<template>  
+<AppHeader/>
 </template>
 
 <style lang="scss">
 @use "./styles/general.scss" as *;
+@use "./styles/partials/mixins" as *;
+@use "./styles/partials/variables" as *;
+
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,15 +2,99 @@
     export default {
         nome: "AppHeader",
         data (){
-            
+            return{
+                links: [
+                    {
+                        name: "charaters",
+                        href: "/charaters",
+                        active: false,
+                    },
+                    {
+                        name: "comics",
+                        href: "/comics",
+                        active: false,
+                    },
+                    {
+                        name: "movies",
+                        href: "/movies",
+                        active: false,
+                    },
+                    {
+                        name: "tv",
+                        href: "/tv",
+                        active: false,
+                    },
+                    {
+                        name: "games",
+                        href: "/games",
+                        active: false,
+                    },
+                    {
+                        name: "collectibles",
+                        href: "/collectibles",
+                        active: false,
+                    },
+                    {
+                        name: "videos",
+                        href: "/videos",
+                        active: false,
+                    },
+                    {
+                        name: "fans",
+                        href: "/fans",
+                        active: false,
+                    },
+                    {
+                        name: "news",
+                        href: "/news",
+                        active: false,
+                    },
+                    {
+                        name: "shop",
+                        href: "/shop",
+                        active: false,
+                    },
+                ]
+            }
+
         }
     }
 </script>
 
 <template>
+    <header>
+        <div class="container flex flex-between">
+
+            <div class="logo"> 
+                <img src="../assets/img/dc-logo.png" alt="logo">
+            </div>
+            <ul class="flex align-center">
+                <li v-for="(link,index) in links"> 
+                    <a :href="link.href">{{link.name}}</a>
+                </li>
+            </ul>
+
+        </div>
+    </header>
 
 </template>
 
 <style lang="scss" scoped>
+header{
+    padding: 1rem;
+    // debug
+    background-color: pink;
+
+    .logo img{
+        width: 60px;
+    }
+
+    ul{
+        gap: 20px;
+    }
+}
+
+
+
 
 </style>
